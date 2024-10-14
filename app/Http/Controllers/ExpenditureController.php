0ea06a376889a8559100cd85c080dd78e8087e8c
james-mathe <kambalemathejacques3o@gmail.com>
2024-10-14 22:23:01 +0200

    es

--- app/Http/Controllers/ExpenditureController.php
+++ app/Http/Controllers/ExpenditureController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Validator;
 class ExpenditureController extends Controller
 {
     /**
+     * 
      * Display a listing of the resource.
      */
     public function index(Request $request)
